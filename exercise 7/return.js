9474dbb82fbd76c5496420f3a1a035930c929985
ArubianLady85 <88424294+ArubianLady85@users.noreply.github.com>
2021-11-28 18:44:26 +0100

    Add files via upload
    Not finished got stuck!! Not happy at all with my result!

---
+++ exercise 7/return.js
@@ -0,0 +1,49 @@
+// No1. Chexking Number above
+let apple = function (number) {
+
+    if (number >= 100) {
+        console.log("True");
+    } else {
+        console.log("false");
+    }
+}
+
+apple(50, 45, 100);
+
+// No2 Bot
+const bouncerBrenda = function (no1, no2, no3) {
+
+    function Check(age, full, enter) {
+
+        if (age >= "21" || full >= "500" || enter <= "500"); {
+            console.log("You may enter");
+            console.log("This is a club for Adults");
+        }
+
+        if (full => 500) {
+            console.log("you may enter");
+        } else {
+            console.log("We are full, please come back later");
+        }
+
+        if (enter <= 500) {
+            console.log("you may enter");
+        } else {
+            console.log("We are full, please come back later");
+        }
+    }
+
+const full();
+const enter();
+const age();
+
+//3 Average
+function grades(a, b, c, d) {
+    const les1 = math.grad(a, 2);
+    const les2 = math.grad(b, 2);
+    const les3 = math.grad(c, 2);
+    const les4 = math.grad(d, 2);
+    return les1 + les2 + les3 + les4;
+}
+
+console.log(grades(8, 7, 10, 4));
